078e60f76b78f47c68805a638536996a2a74b0ab
Jayphen <software@jayphen.com>
2024-02-11 00:37:42 +0100

    Add Fish

---
+++ private_dot_config/private_fish/config.fish
@@ -0,0 +1,19 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
+
+set -gx VOLTA_HOME "$HOME/.volta"
+set -gx GOPATH $HOME/golang
+set -gx GOROOT /opt/homebrew/opt/go/libexec
+
+fish_add_path ~/.local/bin
+fish_add_path ~/.cargo/bin
+fish_add_path $VOLTA_HOME/bin
+fish_add_path $GOPATH/bin
+fish_add_path $GOROOT/bin
+
+# Aliases
+alias cat bat
+alias grep rg
+alias mkdir 'mkdir -p'
+alias cz chezmoi
