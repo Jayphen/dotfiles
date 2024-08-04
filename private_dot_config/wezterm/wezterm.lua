4ab9be5c623c12179102920807156a986f966bdb
Jayphen <software@jayphen.com>
2024-08-04 13:49:33 +1000

    Add wezterm config

---
+++ private_dot_config/wezterm/wezterm.lua
@@ -0,0 +1,17 @@
+-- Pull in the wezterm API
+local wezterm = require("wezterm")
+
+-- This will hold the configuration.
+local config = wezterm.config_builder()
+
+-- This is where you actually apply your config choices
+
+-- For example, changing the color scheme:
+config.color_scheme = "Catppuccin Macchiato"
+
+wezterm.font("Berkeley Mono Variable")
+config.font_size = 14
+config.cell_width = 1
+
+-- and finally, return the configuration to wezterm
+return config
