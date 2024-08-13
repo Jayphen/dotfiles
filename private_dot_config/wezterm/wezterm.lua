c263f5a10c7bdc5835d6746918184972a4bf5cea
Jayphen <software@jayphen.com>
2024-08-13 22:10:09 +1000

    Update wezterm config

--- private_dot_config/wezterm/wezterm.lua
+++ private_dot_config/wezterm/wezterm.lua
@@ -13,5 +13,115 @@ wezterm.font("Berkeley Mono Variable")
 config.font_size = 14
 config.cell_width = 1
 
+-- Slightly transparent and blurred background
+config.window_background_opacity = 0.9
+config.macos_window_background_blur = 30
+-- Removes the title bar, leaving only the tab bar. Keeps
+-- the ability to resize by dragging the window's edges.
+-- On macOS, 'RESIZE|INTEGRATED_BUTTONS' also looks nice if
+-- you want to keep the window controls visible and integrate
+-- them into the tab bar.
+config.window_decorations = "RESIZE"
+-- Sets the font for the window frame (tab bar)
+config.window_frame = {
+	-- Berkeley Mono for me again, though an idea could be to try a
+	-- serif font here instead of monospace for a nicer look?
+	font = wezterm.font({ family = "Menlo", weight = "Bold" }),
+	font_size = 11,
+}
+
+wezterm.on("update-status", function(window)
+	-- Grab the utf8 character for the "powerline" left facing
+	-- solid arrow.
+	local SOLID_LEFT_ARROW = utf8.char(0xe0b2)
+
+	-- Grab the current window's configuration, and from it the
+	-- palette (this is the combination of your chosen colour scheme
+	-- including any overrides).
+	local color_scheme = window:effective_config().resolved_palette
+	local bg = color_scheme.background
+	local fg = color_scheme.foreground
+
+	-- "Wed Mar 3 08:14"
+	local date = wezterm.strftime("%a %b %-d %H:%M ")
+
+	local bat = ""
+	for _, b in ipairs(wezterm.battery_info()) do
+		bat = "🔋 " .. string.format("%.0f%%", b.state_of_charge * 100)
+	end
+
+	window:set_right_status(wezterm.format({
+		-- First, we draw the arrow...
+		{ Background = { Color = "none" } },
+		{ Foreground = { Color = bg } },
+		{ Text = SOLID_LEFT_ARROW },
+		-- Then we draw our text
+		{ Background = { Color = bg } },
+		{ Foreground = { Color = fg } },
+		{ Text = " " .. bat .. "  " .. date .. " " },
+	}))
+end)
+
+local function move_pane(key, direction)
+	return {
+		key = key,
+		mods = "LEADER",
+		action = wezterm.action.ActivatePaneDirection(direction),
+	}
+end
+
+config.keys = {
+	-- ... add these new entries to your config.keys table
+	{
+		key = ",",
+		mods = "SUPER",
+		action = wezterm.action.SpawnCommandInNewTab({
+			cwd = wezterm.home_dir,
+			args = { "nvim", wezterm.config_file },
+		}),
+	},
+	{
+		-- I'm used to tmux bindings, so am using the quotes (") key to
+		-- split horizontally, and the percent (%) key to split vertically.
+		key = "|",
+		-- Note that instead of a key modifier mapped to a key on your keyboard
+		-- like CTRL or ALT, we can use the LEADER modifier instead.
+		-- This means that this binding will be invoked when you press the leader
+		-- (CTRL + A), quickly followed by quotes (").
+		mods = "LEADER",
+		action = wezterm.action.SplitHorizontal({ domain = "CurrentPaneDomain" }),
+	},
+	{
+		key = "\\",
+		mods = "LEADER",
+		action = wezterm.action.SplitVertical({ domain = "CurrentPaneDomain" }),
+	},
+	{
+		key = "a",
+		-- When we're in leader mode _and_ CTRL + A is pressed...
+		mods = "LEADER|CTRL",
+		-- Actually send CTRL + A key to the terminal
+		action = wezterm.action.SendKey({ key = "a", mods = "CTRL" }),
+	},
+	move_pane("j", "Down"),
+	move_pane("k", "Up"),
+	move_pane("h", "Left"),
+	move_pane("l", "Right"),
+	{
+		key = "w",
+		mods = "CMD",
+		action = wezterm.action.CloseCurrentPane({ confirm = true }),
+	},
+}
+
+config.set_environment_variables = {
+	PATH = "/opt/homebrew/bin:" .. os.getenv("PATH"),
+}
+
+-- If you're using emacs you probably wanna choose a different leader here,
+-- since we're gonna be making it a bit harder to CTRL + A for jumping to
+-- the start of a line
+config.leader = { key = "a", mods = "CTRL", timeout_milliseconds = 1000 }
+
 -- and finally, return the configuration to wezterm
 return config
